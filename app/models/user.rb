b3e8e3dafe2a60543b132b1284ca5fd83ab534df
Luke <luke.e.philips@gmail.com>
2017-03-03 09:22:42 -0800

    devise working swell now

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  def self.portfolio
-    byebug
-  end
 end
