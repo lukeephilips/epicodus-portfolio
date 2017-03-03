e5069674fd6267db2b85d34c65ac666c6f4d42c3
Luke <luke.e.philips@gmail.com>
2017-03-02 20:55:06 -0800

    models complete

---
+++ app/models/portfolio.rb
@@ -0,0 +1,8 @@
+class Portfolio < ActiveRecord::Base
+
+  # def initialize(github)
+  #   byebug
+  #   @github = Github.new oauth_token: ENV['GITHUB_TOKEN']
+  # end
+
+end
---
+++ app/models/project.rb
@@ -0,0 +1,6 @@
+class Project < ApplicationRecord
+  def self.repos
+    response = RestClient.get("https://api.github.com/user/repos?access_token=#{ENV['GITHUB_TOKEN']}&affiliation=owner&sort=created")
+    parsed_response = JSON.parse response
+  end
+end
